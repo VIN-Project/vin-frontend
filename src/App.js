3306060418447cabfeaaf84c337296b9717dae10
Sana Jabeen <jsana7399@gmail.com>
2024-09-01 22:47:12 +0500

    facebook login removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import FacebookLogin from 'react-facebook-login';
 import axios from "axios";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import LandingPageRouts from "./pages/routes";
 import VinForm from "./vin-num/VinForm";
 
 function App() {
- const responseFacebook = (response) =>{
- console.log(response);
- }
 
   return (
     <>
@@ -17,10 +13,6 @@ function App() {
         <LandingPageRouts />
         <div>
         <h1>login with facebook</h1>
-        <FacebookLogin
-         appId="1035183138044076"
-         autoLoad={true}
-         callback={responseFacebook} />
         </div>
       </Router>
     </>
